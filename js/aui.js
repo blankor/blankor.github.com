6b9dc6348d356aed06ee0e51a504b7d7cfa7d718
artcss <artskin@163.com>
2013-11-25 16:12:45 +0800

    移动设备兼容解决

--- js/aui.js
+++ js/aui.js
@@ -4,16 +4,25 @@ if ('undefined' == typeof(document.body.style.maxHeight)) {
 
 function autoSize(){
 	var H =$(window).height();
-	$(".vmiddle").css({"padding":H/2-140});
+	$(".vmiddle").css({"padding-top":H/2-140,"padding-bottom":H/2-140,});
 }
 
 $(function() {
 	autoSize();
 	$(".nav a").click(function(){
+		$(".footer").css("position","relative");
 		$(".vmiddle").animate({"padding-top":"5px","padding-bottom":"10px"},300);
-		$(".conter").animate({height: "560px"},300);
+		var conH = $("."+$(this).attr("id")).height()+50;
+		$(".conter").animate({height: conH},300);
 		$(this).addClass("curr").siblings().removeClass("curr");
 		var index = $(this).index();
 		$(".slidew").animate({"margin-left":-index*100+"%"});
 	});
+	
+	//窗口检测
+	$("body").append("<div class='fixednum'></div>");
+	$(".fixednum").html('主体宽：'+$('.layout').width()+'<br>窗口宽：'+$(window).width());
+	$(window).resize(function(){
+		$(".fixednum").html('主体宽：'+$('.layout').width()+'<br>窗口宽：'+$(window).width());
+	})
 });
